2359824cc8a630324f657c34b8a31195eeff039a
piusgori <piusgori@gmail.com>
2022-12-17 15:21:01 +0300

    Remove unused text component

--- instagram-frontend/components/top/Right.js
+++ instagram-frontend/components/top/Right.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Image, TouchableOpacity } from 'react-native'
+import { View, StyleSheet, Image, TouchableOpacity } from 'react-native'
 import React from 'react'
 
 const Right = () => {
